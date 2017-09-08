97a5810a5431e78f64f8cf7da795052deaccd811
chipmc <chip@mcclellands.org>
2017-09-08 15:04:09 -0400

    Version 0.5 - Updates to formatting and documentation

--- src/Cellular-Logger-PIR.ino
+++ src/Cellular-Logger-PIR.ino
@@ -34,7 +34,7 @@
  #define HOURLYCOUNTOFFSET 4         // Offsets for the values in the hourly words
  #define HOURLYBATTOFFSET 6          // Where the hourly battery charge is stored
  // Finally, here are the variables I want to change often and pull them all together here
- #define SOFTWARERELEASENUMBER "0.4"
+ #define SOFTWARERELEASENUMBER "0.5"
  #define PARKCLOSES 19
  #define PARKOPENS 7
 
@@ -111,8 +111,6 @@
    String deviceID = System.deviceID();
    deviceID.toCharArray(responseTopic,125);
    Particle.subscribe(responseTopic, UbidotsHandler, MY_DEVICES);      // Subscribe to the integration response event
-   //Particle.subscribe("hook-response/Hourly_Count", UbidotsHandler, MY_DEVICES);      // Subscribe to the integration response event
-   //Particle.subscribe("hook-response/Daily_Count", UbidotsHandler, MY_DEVICES);      // Subscribe to the integration response event
 
    Particle.variable("HourlyCount", hourlyPersonCount);
    Particle.variable("DailyCount", dailyPersonCount);
@@ -167,50 +165,50 @@
 
  }
 
- void loop() {
-   if(hourlyPersonCountSent && !dataInFlight) {   // Cleared here as there could be counts coming in while "in Flight"
-     hourlyPersonCount -= hourlyPersonCountSent;    // Confirmed that count was recevied - clearing
-     FRAMwrite16(CURRENTHOURLYCOUNTADDR, hourlyPersonCount);  // Load Hourly Count to memory
-     hourlyPersonCountSent = 0;
-   }
-   if(dailyPersonCountSent && !dataInFlight) {
-     hourlyPersonCount -= hourlyPersonCountSent;    // Confirmed that count was recevied - clearing both hourly and daily counts
-     FRAMwrite16(CURRENTHOURLYCOUNTADDR, hourlyPersonCount);  // Load Hourly Count to memory
-     hourlyPersonCountSent = 0;
-     dailyPersonCount -= dailyPersonCountSent;
-     FRAMwrite16(CURRENTDAILYCOUNTADDR,hourlyPersonCount);
-     dailyPersonCountSent = 0;
-   }
-   if (sensorDetect && inTest) {
-     recordCount();
-   }
-   if ((Time.hour() != currentHourlyPeriod) && hourlyPersonCount) {  // Spring into action each hour on the hour as long as we have counts
-     LogHourlyEvent();
-   }
-   if (Time.day() != currentDailyPeriod) {
-     LogDailyEvent();
-   }
- }
+void loop() {
+  if(hourlyPersonCountSent && !dataInFlight) {   // Cleared here as there could be counts coming in while "in Flight"
+    hourlyPersonCount -= hourlyPersonCountSent;    // Confirmed that count was recevied - clearing
+    FRAMwrite16(CURRENTHOURLYCOUNTADDR, hourlyPersonCount);  // Load Hourly Count to memory
+    hourlyPersonCountSent = 0;
+  }
+  if(dailyPersonCountSent && !dataInFlight) {
+    dailyPersonCount -= dailyPersonCountSent;
+    FRAMwrite16(CURRENTDAILYCOUNTADDR,hourlyPersonCount);
+    dailyPersonCountSent = 0;
+  }
+  if (sensorDetect && inTest) {                  // The ISR had raised the sensor flag
+   recordCount();
+  }
+  if (Time.day() != currentDailyPeriod) {     // Time to log a daily event takes precedence over hourly event
+   LogHourlyEvent();
+   LogDailyEvent();
+   sendEvent(1);
+  }
+  if ((Time.hour() != currentHourlyPeriod) && hourlyPersonCount) {  // Spring into action each hour on the hour as long as we have counts
+   LogHourlyEvent();
+   sendEvent(0);
+  }
+}
 
- void recordCount() // This is where we check to see if an interrupt is set when not asleep or act on a tap that woke the Arduino
- {
-   Serial.println(F("It is a tap - counting"));
-   sensorDetect = false;      // Reset the flag
-   lastBump = millis();    // Reset last bump timer
-   t = Time.now();
-   hourlyPersonCount++;                    // Increment the PersonCount
-   FRAMwrite16(CURRENTHOURLYCOUNTADDR, hourlyPersonCount);  // Load Hourly Count to memory
-   dailyPersonCount++;                    // Increment the PersonCount
-   FRAMwrite16(CURRENTDAILYCOUNTADDR, dailyPersonCount);   // Load Daily Count to memory
-   FRAMwrite32(CURRENTCOUNTSTIME, t);   // Write to FRAM - this is so we know when the last counts were saved
-   Serial.print(F("Hourly: "));
-   Serial.print(hourlyPersonCount);
-   Serial.print(F(" Daily: "));
-   Serial.print(dailyPersonCount);
-   Serial.print(F("  Time: "));
-   Serial.println(Time.timeStr(t)); // Prints time t - example: Wed May 21 01:08:47 2014
-   ledState = !ledState;              // toggle the status of the LEDPIN:
-   digitalWrite(blueLED, ledState);    // update the LED pin itself
+void recordCount() // This is where we check to see if an interrupt is set when not asleep or act on a tap that woke the Arduino
+{
+  Serial.println(F("It is a tap - counting"));
+  sensorDetect = false;      // Reset the flag
+  lastBump = millis();    // Reset last bump timer
+  t = Time.now();
+  hourlyPersonCount++;                    // Increment the PersonCount
+  FRAMwrite16(CURRENTHOURLYCOUNTADDR, hourlyPersonCount);  // Load Hourly Count to memory
+  dailyPersonCount++;                    // Increment the PersonCount
+  FRAMwrite16(CURRENTDAILYCOUNTADDR, dailyPersonCount);   // Load Daily Count to memory
+  FRAMwrite32(CURRENTCOUNTSTIME, t);   // Write to FRAM - this is so we know when the last counts were saved
+  Serial.print(F("Hourly: "));
+  Serial.print(hourlyPersonCount);
+  Serial.print(F(" Daily: "));
+  Serial.print(dailyPersonCount);
+  Serial.print(F("  Time: "));
+  Serial.println(Time.timeStr(t)); // Prints time t - example: Wed May 21 01:08:47 2014
+  ledState = !ledState;              // toggle the status of the LEDPIN:
+  digitalWrite(blueLED, ledState);    // update the LED pin itself
  }
 
 
@@ -248,58 +246,52 @@
      }
  }
 
- void LogHourlyEvent() // Log Hourly Event()
- {
-     time_t LogTime = FRAMread32(CURRENTCOUNTSTIME);     // This is the last event recorded - this sets the hourly period
-     unsigned int pointer = (HOURLYOFFSET + FRAMread16(HOURLYPOINTERADDR))*WORDSIZE;  // get the pointer from memory and add the offset
-     LogTime -= (60*Time.minute(LogTime) + Time.second(LogTime)); // So, we need to subtract the minutes and seconds needed to take to the top of the hour
-     FRAMwrite32(pointer, LogTime);   // Write to FRAM - this is the end of the period
-     FRAMwrite16(pointer+HOURLYCOUNTOFFSET,hourlyPersonCount);
-     stateOfCharge = int(batteryMonitor.getSoC());
-     FRAMwrite8(pointer+HOURLYBATTOFFSET,stateOfCharge);
-     unsigned int newHourlyPointerAddr = (FRAMread16(HOURLYPOINTERADDR)+1) % HOURLYCOUNTNUMBER;  // This is where we "wrap" the count to stay in our memory space
-     FRAMwrite16(HOURLYPOINTERADDR,newHourlyPointerAddr);
-     if (SendEvent(1))
-     {
-       hourlyPersonCountSent = hourlyPersonCount; // This is the number that was sent to Ubidots - will be subtracted once we get confirmation
-       dataInFlight = true; // set the data in flight flag
-       currentHourlyPeriod = Time.hour();  // Change the time period
-       Serial.println(F("Hourly Event Sent"));
-     }
- }
+void LogHourlyEvent() // Log Hourly Event()
+{
+  time_t LogTime = FRAMread32(CURRENTCOUNTSTIME);     // This is the last event recorded - this sets the hourly period
+  unsigned int pointer = (HOURLYOFFSET + FRAMread16(HOURLYPOINTERADDR))*WORDSIZE;  // get the pointer from memory and add the offset
+  LogTime -= (60*Time.minute(LogTime) + Time.second(LogTime)); // So, we need to subtract the minutes and seconds needed to take to the top of the hour
+  FRAMwrite32(pointer, LogTime);   // Write to FRAM - this is the end of the period
+  FRAMwrite16(pointer+HOURLYCOUNTOFFSET,hourlyPersonCount);
+  stateOfCharge = int(batteryMonitor.getSoC());
+  FRAMwrite8(pointer+HOURLYBATTOFFSET,stateOfCharge);
+  unsigned int newHourlyPointerAddr = (FRAMread16(HOURLYPOINTERADDR)+1) % HOURLYCOUNTNUMBER;  // This is where we "wrap" the count to stay in our memory space
+  FRAMwrite16(HOURLYPOINTERADDR,newHourlyPointerAddr);
+}
 
  void LogDailyEvent() // Log Daily Event()
  {
-     time_t LogTime = FRAMread32(CURRENTCOUNTSTIME);// This is the last event recorded - this sets the daily period
-     int pointer = (DAILYOFFSET + FRAMread8(DAILYPOINTERADDR))*WORDSIZE;  // get the pointer from memory and add the offset
-     FRAMwrite8(pointer,Time.month(LogTime)); // The month of the last count
-     FRAMwrite8(pointer+DAILYDATEOFFSET,Time.day(LogTime));  // Write to FRAM - this is the end of the period  - should be the day
-     FRAMwrite16(pointer+DAILYCOUNTOFFSET,dailyPersonCount);
-     stateOfCharge = batteryMonitor.getSoC();
-     FRAMwrite8(pointer+DAILYBATTOFFSET,stateOfCharge);
-     byte newDailyPointerAddr = (FRAMread8(DAILYPOINTERADDR)+1) % DAILYCOUNTNUMBER;  // This is where we "wrap" the count to stay in our memory space
-     FRAMwrite8(DAILYPOINTERADDR,newDailyPointerAddr);
-     if (SendEvent(0))
-     {
-       dailyPersonCountSent = dailyPersonCount; // This is the number that was sent to Ubidots - will be subtracted once we get confirmation
-       dataInFlight = true; // set the data in flight flag
-       currentDailyPeriod = Time.day();  // Change the time period
-       Serial.println(F("Daily Event Sent"));
-     }
+   time_t LogTime = FRAMread32(CURRENTCOUNTSTIME);// This is the last event recorded - this sets the daily period
+   int pointer = (DAILYOFFSET + FRAMread8(DAILYPOINTERADDR))*WORDSIZE;  // get the pointer from memory and add the offset
+   FRAMwrite8(pointer,Time.month(LogTime)); // The month of the last count
+   FRAMwrite8(pointer+DAILYDATEOFFSET,Time.day(LogTime));  // Write to FRAM - this is the end of the period  - should be the day
+   FRAMwrite16(pointer+DAILYCOUNTOFFSET,dailyPersonCount);
+   stateOfCharge = batteryMonitor.getSoC();
+   FRAMwrite8(pointer+DAILYBATTOFFSET,stateOfCharge);
+   byte newDailyPointerAddr = (FRAMread8(DAILYPOINTERADDR)+1) % DAILYCOUNTNUMBER;  // This is where we "wrap" the count to stay in our memory space
+   FRAMwrite8(DAILYPOINTERADDR,newDailyPointerAddr);
  }
 
- bool SendEvent(bool hourlyEvent)
+ void sendEvent(bool dailyEvent)
  {
-   // Take the temperature and report to Ubidots - may set up custom webhooks later
+   getSignalStrength();
+   int currentTemp = getTemperature();  // in degrees F
+   stateOfCharge = int(batteryMonitor.getSoC()); // Percentage of full charge
    digitalWrite(donePin, HIGH);
    digitalWrite(donePin,LOW);     // Pet the dog so we have a full period for a response
    doneEnabled = false;           // Can't pet the dog unless we get a confirmation via Webhook Response and the right Ubidots code.
-   int currentTemp = getTemperature(0);  // 0 argument for degrees F
    char data[256];                                         // Store the date in this character array - not global
    snprintf(data, sizeof(data), "{\"hourly\":%i, \"daily\":%i,\"battery\":%i, \"temp\":%i}",hourlyPersonCount, dailyPersonCount, stateOfCharge, currentTemp);
-   if (hourlyEvent) Particle.publish("Hourly_Count", data, PRIVATE);
-   else Particle.publish("Daily_Count", data, PRIVATE);
-   return 1;
+   Particle.publish("Send_Counts", data, PRIVATE);
+   if (dailyEvent)
+   {
+     dailyPersonCountSent = dailyPersonCount; // This is the number that was sent to Ubidots - will be subtracted once we get confirmation
+     currentDailyPeriod = Time.day();  // Change the time period
+   }
+   hourlyPersonCountSent = hourlyPersonCount; // This is the number that was sent to Ubidots - will be subtracted once we get confirmation
+   dataInFlight = true; // set the data in flight flag
+   currentHourlyPeriod = Time.hour();  // Change the time period
+   Serial.println(F("Event Sent"));
  }
 
 void UbidotsHandler(const char *event, const char *data)  // Looks at the response from Ubidots - Will reset Photon if no successful response
@@ -322,25 +314,7 @@ void UbidotsHandler(const char *event, const char *data)  // Looks at the respon
   else Particle.publish("UbidotsHook", data);             // Publish the response code
 }
 
-void NonBlockingDelay(int millisDelay)  // Used for a non-blocking delay
-{
-    unsigned long commandTime = millis();
-    while (millis() <= millisDelay + commandTime) {
-      Particle.process();
-    }
-    return;
-}
 
-void BlinkForever() // When something goes badly wrong...
-{
-    Serial.println(F("Error - Cannot Proceed"));
-    while(1) {
-        digitalWrite(blueLED,HIGH);
-        delay(200);
-        digitalWrite(blueLED,LOW);
-        delay(200);
-    }
-}
 
 void getSignalStrength()
 {
@@ -399,36 +373,20 @@ int sendNow(String command) // Function to force sending data in current hour
 {
   if (command == "1")
   {
-    getSignalStrength();
-    LogHourlyEvent();
+    sendEvent(0);
     return 1;
   }
   else return 0;
 }
 
-
- int getTemperature(bool degC)
+int getTemperature()
 {
-  //getting the voltage reading from the temperature sensor
-  int reading = analogRead(tmp36Pin);
-
-  // converting that reading to voltage, for 3.3v arduino use 3.3
-  float voltage = reading * 3.3;
-  voltage /= 4096.0;        // This is different than the Arduino where there are only 1024 steps
-
-  // now print out the temperature
+  int reading = analogRead(tmp36Pin);   //getting the voltage reading from the temperature sensor
+  float voltage = reading * 3.3;        // converting that reading to voltage, for 3.3v arduino use 3.3
+  voltage /= 4096.0;                    // Electron is different than the Arduino where there are only 1024 steps
   int temperatureC = int(((voltage - 0.5) * 100));  //converting from 10 mv per degree with 500 mV offset to degrees ((voltage - 500mV) times 100) - 5 degree calibration
-  // now convert to Fahrenheit
-  temperatureF = int((temperatureC * 9.0 / 5.0) + 32.0);
-
-  if (degC)
-  {
-      return temperatureC;
-  }
-  else
-  {
-      return temperatureF;
-  }
+  temperatureF = int((temperatureC * 9.0 / 5.0) + 32.0);  // now convert to Fahrenheit
+  return temperatureF;
 }
 
 void watchdogISR()
@@ -443,4 +401,25 @@ void watchdogISR()
 void sensorISR()
 {
   sensorDetect = true;  // sets the sensor flag for the main loop
+}
+
+
+void NonBlockingDelay(int millisDelay)  // Used for a non-blocking delay
+{
+    unsigned long commandTime = millis();
+    while (millis() <= millisDelay + commandTime) {
+      Particle.process();
+    }
+    return;
+}
+
+void BlinkForever() // When something goes badly wrong...
+{
+    Serial.println(F("Error - Cannot Proceed"));
+    while(1) {
+        digitalWrite(blueLED,HIGH);
+        delay(200);
+        digitalWrite(blueLED,LOW);
+        delay(200);
+    }
 }
